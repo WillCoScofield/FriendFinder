f76dfa6c7253af5b4cf5527b9f072132a514c9db
Connor <wscofield@elon.edu>
2018-04-16 11:28:46 -0400

    Friends Array Created, 2 json Objects in array

---
+++ data/friends.js
@@ -0,0 +1,43 @@
+
+
+//Array of Friends to be matched against
+
+var friendsArray = [
+    {
+        "name": "Baby Face Bill",
+        "photo": "http://www.funnyfacespictures.net/pictures/funny-faces-baby_face.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
+    {
+        "name": "Average Joe",
+        "photo": "http://www.jewwatch.com/images/Entertainment%20Pics/gif-mesh-adam-the-average-joe-tv-show.jpg",
+        "scores": [
+            2,
+            1,
+            5,
+            3,
+            1,
+            2,
+            5,
+            3,
+            2,
+            15
+        ]
+    }
+
+
+]
+
+module.exports = friendsArray;
